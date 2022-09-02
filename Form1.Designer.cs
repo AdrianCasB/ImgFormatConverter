c41fc4b73800a86e512bab8f7f590e2a12fc1000
AdrianCasB <112508540+AdrianCasB@users.noreply.github.com>
2022-09-02 02:24:18 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication2_AdrianCastillo_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Guardar como";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "*.bmp",
+            "*.jpg",
+            "*.gif",
+            "*.tiff"});
+            this.comboBox1.Location = new System.Drawing.Point(86, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Seleccionar formato";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "*.bmp",
+            "*.jpg",
+            "*.gif",
+            "*.tiff"});
+            this.comboBox2.Location = new System.Drawing.Point(382, 14);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(127, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Seleccionar formato";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(504, 451);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 611);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Conversor formatos imagen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2_AdrianCastillo_
+{
+    public partial class Form1 : Form
+    {
+        Image img1;
+        Bitmap Image1;
+        String ruta;
+        private Size oldSize;
+        private SaveFileDialog saveFileDialog;
+        private void Form1_Load(object sender, EventArgs e) => oldSize = base.Size;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.saveFileDialog = new SaveFileDialog();
+           saveFileDialog.Filter = this.comboBox2.Text + "|" + this.comboBox2.Text; ;
+            DialogResult drd = saveFileDialog.ShowDialog();
+        
+            if (drd == DialogResult.OK ) ;
+            {
+
+                //MÉTODO CON FILESTREAM + SWITCH
+
+                /*  
+                    System.IO.FileStream fs =
+                       (System.IO.FileStream)saveFileDialog.OpenFile();
+                 String formato = comboBox2.Text.ToString();
+                    switch (formato)
+                    {
+                        case "*.jpg":
+                            img1.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+
+                        case "*.bmp":
+                            img1.Save(fs,System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+
+                        case "*.gif":
+                            img1.Save(fs,System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+
+                        case "*.tiff":
+                            img1.Save(fs,System.Drawing.Imaging.ImageFormat.Tiff);
+                            break;
+                    }
+                  fs.Close();
+                 /*
+
+                 //MÉTODO CON FILESTREAM + IF ELSE
+
+                 /* 
+                  System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog.OpenFile();
+
+                   if (this.comboBox2.Text == "*.bmp")
+                  {
+                      img1.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                  }
+                  else if (this.comboBox2.Text == "*.jpg")
+                  {
+                      img1.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                  }else if (this.comboBox2.Text == "*.gif")
+                  {
+                      img1.Save(fs, System.Drawing.Imaging.ImageFormat.Gif);
+                  }else if(this.comboBox2.Text == "*.tiff")
+                  {
+                      img1.Save(fs, System.Drawing.Imaging.ImageFormat.Tiff);
+                  }
+
+                  fs.Close();*/
+
+
+
+                //MÉTODO CON IF SIN FILESTREAM
+
+                /* String formato = comboBox2.Text.ToString();
+                  if (formato == "*.bmp")
+                  {
+                      img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                  } else if ( formato == "*.jpg")
+                  {
+                      img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                  }else if ( formato == "*.tiff")
+                  {
+                      img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+
+                  }else if ( formato == "*.gif")
+                  {
+                      img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                  }*/
+
+
+
+
+                //MÉTODO CON SWITCH SIN FILESTREAM
+
+                String formato = comboBox2.Text.ToString();
+                switch (formato)
+                {
+                    case "*.jpg":
+                        img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case "*.bmp":
+                        img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case "*.gif":
+                        img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+
+                    case "*.tiff":
+                        img1.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                        break;
+                }
+
+            }
+
+
+            string text = "Imagen convertida con éxito"; 
+            MessageBox.Show(text); //VENTANA EMERGENTE
+
+        }
+    
+           
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = this.comboBox1.Text + "|" + this.comboBox1.Text;
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK )
+            {
+                this.ruta = ofd.FileName;
+
+                Image1 = new Bitmap(ruta);
+
+                pictureBox1.Image = Image1;
+                this.img1 = Image1;
+
+                int Altura = Image1.Height;
+                int Anchura = Image1.Width;
+
+                Size = new Size(Altura + 200, Anchura + 200);
+
+              
+
+              
+
+            }
+
+           
+            
+            
+        }
+       
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnResize(System.EventArgs e) 
+        {
+            base.OnResize(e);
+
+            foreach (Control cnt in this.Controls)
+                ResizeAll(cnt, base.Size);
+
+            oldSize = base.Size;
+        }
+        private void ResizeAll(Control control, Size newSize)
+        {
+            int width = newSize.Width - oldSize.Width;
+            control.Left += (control.Left * width) / oldSize.Width;
+            control.Width += (control.Width * width) / oldSize.Width;
+
+            int height = newSize.Height - oldSize.Height;
+            control.Top += (control.Top * height) / oldSize.Height;
+            control.Height += (control.Height * height) / oldSize.Height;
+        }
+
+        
+    }
+}
